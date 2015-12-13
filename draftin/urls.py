8085b4bebd03b12f28890b6e787f7acd4ba5ae97
Jason Goldstein <jason@betheshoe.com>
2015-12-13 16:24:37 -0500

    urlpatterns is deprecated

--- draftin/urls.py
+++ draftin/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from .views import endpoint
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^(?P<uuid>[-\w\d]+)/$', endpoint, name="draftin.endpoint"),
-)
+]
